e0a9dd4bc42330af645c92ace2a6d4363a359dff
hismacgyver <hismacgyver@gmail.com>
2020-10-13 15:49:30 +0900

    Initial commit

---
+++ src/main/java/com/ds/freeboard/web/dto/HelloResponseDto.java
@@ -0,0 +1,12 @@
+package com.ds.freeboard.web.dto;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public class HelloResponseDto {
+
+    private final String name;
+    private final int amount;
+}
