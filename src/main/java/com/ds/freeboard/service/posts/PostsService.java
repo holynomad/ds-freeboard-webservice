f5071c2a0827e0e9d379fd5d648a35f14cd2c5b0
hismacgyver <daniel81.lee@gmail.com>
2020-10-20 00:26:50 +0900

    add API Contorller/Service/Request(Post) DTO

---
+++ src/main/java/com/ds/freeboard/service/posts/PostsService.java
@@ -0,0 +1,18 @@
+package com.ds.freeboard.service.posts;
+
+import com.ds.freeboard.domain.posts.PostsRepository;
+import com.ds.freeboard.web.dto.PostsSaveRequestDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@RequiredArgsConstructor
+@Service
+public class PostsService {
+    private final PostsRepository postsRepository;
+
+    @Transactional
+    public Long save(PostsSaveRequestDto requestDto) {
+        return postsRepository.save(requestDto.toEntity()).getId();
+    }
+}
